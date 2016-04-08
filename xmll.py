7e179e0c314f585e3b5386a40041e0d0e31da219
MortalCatalyst <flebber.crue@gmail.com>
2016-04-08 19:21:33 +1000

    first commit

---
+++ xmll.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import xmltodict
+
+
+with open('20160319RHIL0.xml') as f:
+    my = xmltodict.parse(f.read())
+    for event in my[1]:
+        eventID = event["@id"]
+        location = event["@venue"]
+        eventDate = event["@date"]
+        print( eventID + '\t' + location + '\t' + eventDate)
+        for race in my['meeting']['race']:
+            print(race["@id"] + '\t' + race["@number"])
+            for noms in race['nomination']:
+                saddle = noms["@number"]
+                horse = noms["@horse"]
+                horseId = noms["@id"]
+                print(saddle + '\t' + horse + '\t' + horseId)
